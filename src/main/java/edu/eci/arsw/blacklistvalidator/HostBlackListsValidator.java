9c350b5708bcb94b4cf1edd529a7c3da36516dbb
Daniel R <daniel.rincon-m@mail.escuelaing.edu.co>
2021-01-21 09:56:28 -0500

    Parte 1 del segundo punto

--- src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
@@ -6,6 +6,8 @@
 package edu.eci.arsw.blacklistvalidator;
 
 import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -29,27 +31,35 @@ public class HostBlackListsValidator {
      * @param ipaddress suspicious host's IP address.
      * @return  Blacklists numbers where the given host's IP address was found.
      */
-    public List<Integer> checkHost(String ipaddress){
-        
-        LinkedList<Integer> blackListOcurrences=new LinkedList<>();
-        
-        int ocurrencesCount=0;
-        
+    public List<Integer> checkHost(String ipaddress, int N) {
+        LinkedList<Integer> blackListOcurrences = new LinkedList<>();
         HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
-        
-        int checkedListsCount=0;
-        
-        for (int i=0;i<skds.getRegisteredServersCount() && ocurrencesCount<BLACK_LIST_ALARM_COUNT;i++){
-            checkedListsCount++;
-            
-            if (skds.isInBlackListServer(i, ipaddress)){
-                
-                blackListOcurrences.add(i);
-                
-                ocurrencesCount++;
+        ArrayList<ThreadValidator> validators = new ArrayList<>();
+
+        int ocurrencesCount = 0;
+        int checkedListsCount = 0;
+        int serverNumber = skds.getRegisteredServersCount();
+
+        for (int i = 0; i < N; i++){
+            int lowerLimit = serverNumber / N * i;
+            int upperLimit = i == N - 1 ? serverNumber - 1 : serverNumber / N * (i + 1) - 1;
+            ThreadValidator validator = new ThreadValidator(lowerLimit, upperLimit, ipaddress);
+            validators.add(validator);
+            validator.start();
+        }
+        for (ThreadValidator validator : validators) {
+            try {
+                validator.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-        
+        for (ThreadValidator validator : validators) {
+            blackListOcurrences.addAll(validator.getOcurrences());
+            checkedListsCount += validator.getCheckedListsCount();
+        }
+        ocurrencesCount = blackListOcurrences.size();
+
         if (ocurrencesCount>=BLACK_LIST_ALARM_COUNT){
             skds.reportAsNotTrustworthy(ipaddress);
         }
