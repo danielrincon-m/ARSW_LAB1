f74dfbea2984d090bec243ed0266955a045989fc
Daniel R <daniel.rincon-m@mail.escuelaing.edu.co>
2021-01-21 08:21:24 -0500

    Primer punto y corrección de archivo raro

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,19 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread extends Thread {
+
+    private final int a;
+    private final int b;
+
+    public CountThread(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void run() {
+        for (int i = a; i <= b; i++) {
+            System.out.println(i);
+        }
+    }
 }
