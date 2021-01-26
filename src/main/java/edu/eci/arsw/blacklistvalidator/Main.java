03c5159b77b74661c0df06f3263ad8a8847b1dd6
Daniel R <daniel.rincon-m@mail.escuelaing.edu.co>
2021-01-25 21:35:14 -0500

    Código para encontrar el número de nucleos

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -15,7 +15,9 @@ public class Main {
     
     public static void main(String a[]){
         HostBlackListsValidator hblv=new HostBlackListsValidator();
-        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55", 100);
+        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55", 50);
+//        int processorNumber = Runtime.getRuntime().availableProcessors();
+//        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55", 2 * processorNumber);
         System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
         
     }
